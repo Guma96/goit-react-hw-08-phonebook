faf4b63038da2394771b1eb15533292487416057
Guma96 <mateusz.kutarba96@gmail.com>
2023-07-23 13:44:42 +0200

    2
    123

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,12 +3,7 @@ import c from './HomePage.module.css';
 const HomePage = () => {
   return (
     <div className={c.wrapper}>
-      <h1 className={c.title}>
-        Phonebook Welcome Page{' '}
-        <span role="img" aria-label="Telephone icon">
-          ☎️
-        </span>
-      </h1>
+      <h1 className={c.title}>Phonebook Welcome Page </h1>
       <p>Please Register or Login</p>
     </div>
   );
